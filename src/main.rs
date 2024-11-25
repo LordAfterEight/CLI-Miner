63168f539242bc6448158d244b39aa509050e036
LordAfterEight <elias@stettmayer.de>
2024-11-25 23:27:25 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal,
+};
+
+fn main() {
+    println!("Hello, world!");
+}
